caa5d96dfbe18e088c7cde258b640bbf6a0dd894
kushalshrestha <kushalshr@gmail.com>
2023-02-04 14:10:10 -0600

    feat: BookController added

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# Project Notes
+
+Table: Author (firstName: String | lastName: String)
+
+Table: Book (title: String | isbn: String)
+* Many to Many Relationship:
+* One author can have multiple boo
+* One book can have multiple authors
+
+Table: Publisher
+* One Publisher can have multiple books
+
+### Spring Data Repositories
+Spring Data is a family of projects, implements a repository pattern.
---
+++ src/main/java/kushal/spring5webapp/controllers/BookController.java
@@ -0,0 +1,20 @@
+package kushal.spring5webapp.controllers;
+
+import kushal.spring5webapp.repositories.BookRepository;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+public class BookController {
+
+    private final BookRepository bookRepository;
+
+    public BookController(BookRepository bookRepository) {
+        this.bookRepository = bookRepository;
+    }
+
+    @RequestMapping("/books")
+    public String getBooks(Model model) {
+        model.addAttribute("books", bookRepository.findAll());
+        return "books";
+    }
+}
