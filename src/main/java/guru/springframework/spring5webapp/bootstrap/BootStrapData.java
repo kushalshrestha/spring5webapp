f049d77149797b938734afa948a494ac5c09d64d
kushalshrestha <kushalshr@gmail.com>
2023-02-03 18:00:17 -0600

    Publisher class, repository created and sample data added from bootstrap

--- src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
+++ src/main/java/guru/springframework/spring5webapp/bootstrap/BootStrapData.java
@@ -2,8 +2,10 @@ package guru.springframework.spring5webapp.bootstrap;
 
 import guru.springframework.spring5webapp.domain.Author;
 import guru.springframework.spring5webapp.domain.Book;
+import guru.springframework.spring5webapp.domain.Publisher;
 import guru.springframework.spring5webapp.repositories.AuthorRepository;
 import guru.springframework.spring5webapp.repositories.BookRepository;
+import guru.springframework.spring5webapp.repositories.PublisherRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
@@ -12,16 +14,27 @@ public class BootStrapData implements CommandLineRunner {
 
     private final AuthorRepository authorRepository;
     private final BookRepository bookRepository;
+    private final PublisherRepository publisherRepository;
 
-    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+    public BootStrapData(AuthorRepository authorRepository, BookRepository bookRepository ,PublisherRepository publisherRepository) {
         this.authorRepository = authorRepository;
         this.bookRepository = bookRepository;
+        this.publisherRepository = publisherRepository;
     }
 
     @Override
     public void run(String... args) throws Exception {
+        System.out.println("Started in Bootstrap");
         Author kushal = new Author("Kushal", "Shrestha");
         Book b1 = new Book("Java 8", "123456");
+        Publisher publisher = new Publisher();
+        publisher.setName("SFG Publishing");
+        publisher.setCity("St Petersburg");
+        publisher.setState("FL");
+
+        publisherRepository.save(publisher);
+
+
         kushal.getBooks().add(b1);
         b1.getAuthors().add(kushal);
 
@@ -36,7 +49,7 @@ public class BootStrapData implements CommandLineRunner {
         authorRepository.save(anil);
         bookRepository.save(b2);
 
-        System.out.println("Started in Bootstrap");
+
         System.out.println("Number of books :" + bookRepository.count());
     }
 }
